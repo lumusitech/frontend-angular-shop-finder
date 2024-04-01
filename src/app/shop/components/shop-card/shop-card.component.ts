ca7c99eb0be7add91d72c8b2bec83d66d2d47069
luciano <lumusitech@gmail.com>
2024-04-01 13:59:19 -0300

    feat: add navigation from shop card to details page

--- src/app/shop/components/shop-card/shop-card.component.ts
+++ src/app/shop/components/shop-card/shop-card.component.ts
@@ -1,13 +1,19 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import type { Shop } from '@app/shop/models/shop.interface';
 
 @Component({
   selector: 'shop-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './shop-card.component.html',
   styles: ``,
 })
 export class ShopCardComponent {
+  router = inject(Router);
   shop = input.required<Shop>();
+
+  onclick() {
+    this.router.navigate(['shop', this.shop().id]);
+  }
 }

08c726427422814b7b65799e91c2ac467b8631bd
luciano <lumusitech@gmail.com>
2024-04-01 13:33:28 -0300

    feat: add router link to logo button for redirect to home

--- src/app/shared/components/buttons/logo-button/logo-button.component.ts
+++ src/app/shared/components/buttons/logo-button/logo-button.component.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { ShopIconComponent } from '../../icons/shop-icon/shop-icon.component';
 
 @Component({
   selector: 'shared-logo-button',
   standalone: true,
-  imports: [ShopIconComponent],
+  imports: [ShopIconComponent, RouterLink],
   templateUrl: './logo-button.component.html',
   styles: ``,
 })

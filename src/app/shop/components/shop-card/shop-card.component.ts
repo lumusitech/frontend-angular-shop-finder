16d8a14b7f7e7beae4e5130dfe6c104c46f102b0
Luciano Figueroa <lumusitech@gmail.com>
2024-04-03 21:20:05 -0300

    refactor: move shop card bottom icons to a svg icon component

--- src/app/shop/components/shop-card/shop-card.component.ts
+++ src/app/shop/components/shop-card/shop-card.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject, input } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { SvgIconComponent } from '@app/shared/components/icons/svg-icon/svg-icon.component';
 import type { Shop } from '@app/shop/models/shop.interface';
 
 @Component({
   selector: 'shop-card',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, SvgIconComponent],
   templateUrl: './shop-card.component.html',
   styles: ``,
 })
